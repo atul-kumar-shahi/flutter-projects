10275071a519006129d6791de9396b3c755eb742
AtuL Kumar <atulshahi2003@gmail.com>
2023-10-31 00:06:38 +0530

    added different section for gradien color

---
+++ demo_app/lib/gradien_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GradientColor extends StatelessWidget {
+  const GradientColor({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 88, 173, 129),
+            Color.fromARGB(255, 21, 178, 97),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomLeft,
+        ),
+      ),
+      child: const Center(
+        child: Text("Hello world",
+            style: TextStyle(color: Colors.white, fontSize: 28)),
+      ),
+    );
+  }
+}
--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -1,33 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:demo_app/gradien_colors.dart';
 
-void main(){
-  runApp(const MaterialApp(
-    home:Scaffold(
-      body:GradientColor(),
-    )
-  ),);
+void main() {
+  runApp(
+    const MaterialApp(
+        home: Scaffold(
+      body: GradientColor(),
+    )),
+  );
 }
-
-class GradientColor extends StatelessWidget {
-  const GradientColor({super.key});
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 88, 173, 129),
-            Color.fromARGB(255, 21, 178, 97),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomLeft,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-            "Hello world", style: TextStyle(color: Colors.white, fontSize: 28)),
-      ),
-    );
-  }
-}
-
