12aa72aee5f841e16daecea8db6237408b4c0116
AtuL Kumar <atulshahi2003@gmail.com>
2023-11-06 20:36:21 +0530

    ExpenseTracker

---
+++ expensetracker/lib/widgets/expenses_list/expense_items.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
