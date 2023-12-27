07202880d9a152417b2b77e4474d62d2bfae8c23
AtuL Kumar <atulshahi2003@gmail.com>
2023-12-27 16:22:19 +0530

    add interaction with camera and stored the picture taken from camera

---
+++ favorite_places/lib/screeen/add_place.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
+
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController=TextEditingController();
+  File ? _selectedImage;
+
+  void _savePlace(){
+    final enteredText=_titleController.text;
+    if(enteredText.isEmpty||_selectedImage==null)
+      {
+        return ;
+      }
+    ref.read(userPlacesProvider.notifier).addPlace(enteredText,_selectedImage!);
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+   _titleController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new Place'),
+      ),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Title'
+
+              ),
+              style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+              controller:_titleController ,
+            ),
+            const SizedBox(height: 10,),
+            ImageInput(onPickImage: (image){
+              _selectedImage= image;
+            },),
+            const SizedBox(height: 16,),
+            ElevatedButton.icon(onPressed: _savePlace, label: const Text('Add Place'),icon: const Icon(Icons.add),),
+          ],
+        ),
+      ),
+    );
+  }
+}
